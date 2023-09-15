06af7419dfa06ac3a04700cd5fa2fd8e7363371f
ejmcill73 <144815634+ejmcill73@users.noreply.github.com>
2023-09-15 14:25:11 +0000

    done quiz

--- quiz1.py
+++ quiz1.py
@@ -1,35 +1,53 @@
 # 1. In your on words, describe what a variable is.
- 2. Name three (3) operators we've learned about in class and describe 
+"a value that can change depending on the conditions of the program" 
+
+# 2. Name three (3) operators we've learned about in class and describe 
 # a situation where you would use each one. 
+"operation 1 Addition  Operation 2 subtraction Operation 3 Multiplication
+Operation 1 example a+b operation 2 example a-b operation 3 example a*b"
 
 # 3. Describe the difference between camal case and pascal case.
+"Camel case is the second word of a variable name and every word after starts with a capital letter and pascal case is every word including the first word, starts with a capital letter"
 
 # 4. Which operator is being used in the example below
 numberOfApples = 10
 numberOfOranges = 12
 
 numberOfApples < numberOfOranges
+"the operater being used is equal to"
 
 # 5. You have been given a $100.00 dollar gift card from a family member. Another 
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
+family_gift_1=100.00
+family_gift=50.00
+friend_gift=35.50
 
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+user_name=input("please enter your name")
+Print("Hello," + user_name+"!")
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
 # They want to know a visitor's full name, their age, if they have an allergy, and what their 
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
+full_name=input("Please enter your full name:  ")
+age  = input ("please enter your age: ")
+has_allergy= input("do you have any allergies?(Yes/No)")
 
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
+has_gluten_allergy=True 
 
 # 9. In your own words, describe what a program is?
+"coded software"
 
 # 10. Describe the difference between a float and an integer. 
+"A floar is a number that has a decimal"
+"An interger is any whole number "
