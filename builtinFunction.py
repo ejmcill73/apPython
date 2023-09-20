97abed096a9320ab0703e6804e9a3f354b67ad3b
ejmcill73 <144815634+ejmcill73@users.noreply.github.com>
2023-09-20 15:24:24 +0000

    9/20 work

--- builtinFunction.py
+++ builtinFunction.py
@@ -8,3 +8,10 @@ print(type(car))
 is_it_a_week_day = True 
 
 number_of_books = 10 
+age=110 
+
+print(type(age))
+ type()used to return the data type of the value passed inside the curly brackets 
+float()converts any interger value passed  inside of curly brackets into a float data type
+int()converts any float value passed inside of the curly brackets into an interger  data type
+str()converts any value passed inside of the curly brackets into a string data type
