e95e44d5e2222f5a6b57db185e3ab6284b2c950d
ejmcill73 <144815634+ejmcill73@users.noreply.github.com>
2023-09-22 18:19:59 +0000

    Friday 9/22

---
+++ ap python activities/FunctionStarterActiviy.py
@@ -0,0 +1,32 @@
+# 1. In your own words, describe what a function is
+#a block of code which only runs when it is called
+
+# 2. A parameter is a variable in a function definition. It is a placeholder and hence does not have a concrete value. An argument is a value passed during function invocation
+# 3. write a function that will print out a welcome message
+# that includes a users name. You will need to use parameters and arguments
+def welcome_user(user_name):
+    print(f"Welcome, {user_name}! We're glad to have you.")
+
+# 4. Write a function that will do a calculation that takes 3 parameters.
+# Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
+def calculate(num1, num2, operator )
+if operator == '+':
+    result= num1+ num2
+elif operator =='-' :
+    result= num1 - num2
+elif operator =='*':
+    result = num1 * num2
+elif operator =='/' :
+    if num2 ! = 0: 
+result = num1 / num2 
+else:
+# 5. Write a function that will output a message to a user, telling them
+# what class they have next after this one. this code should use a 
+# variable to pass a value into the parameter of a function. The variable should
+# be real, user data- not hard coded data.
+users_schedule = 
+"8:00 AM" :"Math"
+"9:30 AM" : "History"
+"11:00 AM": "Science"
+"1:30 PM":  "English"
+"3:00 PM": "GYM"
