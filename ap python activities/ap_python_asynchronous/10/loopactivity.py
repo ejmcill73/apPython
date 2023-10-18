5a10a727f3c75cf0e7131a132540eb4d9a0344fe
ejmcill73 <144815634+ejmcill73@users.noreply.github.com>
2023-10-18 15:19:38 +0000

    work 10/18

---
+++ ap python activities/ap_python_asynchronous/10/loopactivity.py
@@ -0,0 +1,36 @@
+# Clothes are wet 
+moisture_level = 100
+# use dryer untill dry
+while moisture_level > 0: 
+    moisture_level -= 5
+    #once the loop finishes , clothes are dry 
+    print("clothes are dry!")
+
+    # Create a security camera program that uses a while loop to detect if there is an
+# object in site. 
+
+# Create a Printer Loop program that will continue to print copies of a document based on the number
+# that the user inputs. 
+while True: 
+    num_copies = input("enter the number of copies to print(or exit' to quit):  ")
+    if num_copies.lower() == 'exit'; 
+        print(f"Printing {num_copies} copies...")
+    except ValueError:
+print("Please enter a valid number or 'exit".)
+
+# Create a Stop Light Loop that will change the light color based on different time intervals. 
+# every 30 seconds the light should change between green and red. 
+import time 
+while true 
+Print("Light is red")
+time(30)
+print("Light is green")
+time(30)
+print("Light is yellow")
+time(5)
+
+# BONUS: add an additional condition that will change the light to yellow for 5 seconds before the
+# next light change. 
+
+
+
