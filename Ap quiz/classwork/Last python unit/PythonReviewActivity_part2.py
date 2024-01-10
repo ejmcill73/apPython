35b577ae44f1119568d20fb56baf47da280e8efa
ejmcill73 <144815634+ejmcill73@users.noreply.github.com>
2024-01-10 16:19:50 +0000

    work/ 1/10/23

---
+++ Ap quiz/classwork/Last python unit/PythonReviewActivity_part2.py
@@ -0,0 +1,13 @@
+def mad_lib(): 
+    noun1= input("enter noun")
+    verb1= input("Enter a verb")
+    adjective= input ("Enter an Adjective")
+
+
+    noun2= input("Enter another noun")
+    verb2= input("Enter another verb")
+    adjective2= input("Enter another adjective")
+
+    mad_lib= ("on christmas,i got") (noun1)
+
+
---
+++ Ap quiz/classwork/Last python unit/pythonreview.py
@@ -0,0 +1,5 @@
+1.  # a computer program is a seires of instructions given to a program to act  out a certain task 
+
+2. #integer, an integer is a whole number you use integers when you need a whole number without a decimal. Python List, You use a python List to group a series of strings into a list to use in your program. Float, You use the float data type when you need to a decimal instead of a whole number.
+
+3. # The function you waould use to input user data would be the input() function.
